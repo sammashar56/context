e07ef18344a2e6b711dd6e7a7c9873316c0aec0b
Samuel Mwangi <machar@localhost.localdomain>
2020-03-20 14:16:16 -0400

    last changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export const Home = () => {
         <Fragment>
             <div className="App">
                 <div className="container mx-auto">
-                    <h3 className="text-center  text-3xl mt-20 text-base leading-8 text-black font-bold tracking-wide uppercase">CRUD with React Context API and Hooks</h3>
+                    <h3 className="text-center  text-3xl mt-20 text-base leading-8 text-black font-bold tracking-wide uppercase">CRUD context Api</h3>
                     <Heading />
                     <Employeelist />
                 </div>
